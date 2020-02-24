87fd8c2349839bc566b2cd2e93dc8ad57a0cbaf5
KN <kylenguyen@Kyles-MacBook-Pro-2.local>
2020-02-24 12:37:28 -0800

    Proxy endpoints set up

--- JD-proxy/server.js
+++ JD-proxy/server.js
@@ -4,15 +4,14 @@ const axios = require('axios')
 const app = express()
 const port = 3000
 
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
-app.get('/seed', (req, res) => {
-  axios.get('http://18.223.108.8:3003/seed')
+// Get a home listings data
+app.get('/api/listing/:id/data', (req, res) => {
+  let id = req.params.id
+  axios.get(`http://localhost:3002/api/listing/${id}/data`)
     .then(response => {
       res.send(response.data);
     })
@@ -20,9 +19,10 @@ app.get('/seed', (req, res) => {
       console.log(err)
     })
 });
-
-app.get('/houses', (req, res) => {
-  axios.get('http://18.144.88.205:3004/houses')
+// Get a agents data
+app.get('/api/agents/:id/data', (req, res) => {
+  let id = req.params.id
+  axios.get(`http://localhost:3002/api/agents/${id}/data`)
     .then(response => {
       res.send(response.data);
     })
@@ -30,26 +30,25 @@ app.get('/houses', (req, res) => {
       console.log(err)
     })
 });
-
-app.get('/listings', (req, res) => {
-  axios.get('http://52.53.150.79:3001/listings')
+// Post a new listing
+app.post('/api/listing', (req, res) => {
+  axios.post('http://localhost:3002/api/listing', req.body)
     .then(response => {
       res.send(response.data);
     })
     .catch(err => {
-      console.log(err)
+      console.log(err);
     })
-});
-
-app.get('/api/summary/data/:id', (req, res) => {
-  const id = req.params.id;
-  axios.get(`http://3.17.76.247:3002/api/summary/data/${id}`)
-  .then((response) => {
-    res.send(response.data);
-  })
-  .catch((error) => {
-    console.log(error);
-  })
-});
+})
+// Post a new agent
+app.post('/api/agents', (req, res) => {
+  axios.post('http://localhost:3002/api/agents', req.body)
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
